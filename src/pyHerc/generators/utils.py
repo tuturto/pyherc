a46b8bd34087c2cf850376a1bb983f2b1b3ece81
Tuukka Turto <tuukka.turto@oktaeder.net>
2010-11-27 20:46:28 +0200

    working with level creation

---
+++ src/pyHerc/generators/utils.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   Copyright 2010 Tuukka Turto
+#
+#   This file is part of pyHerc.
+#
+#   pyHerc is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   pyHerc is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with pyHerc.  If not, see <http://www.gnu.org/licenses/>.
+
+import random
+import logging
+
+class BSPSection:
+    """
+    Class used to divide area in sections
+    """
+    def __init__(self):
+        self.corner1 = None
+        self.corner2 = None
+        self.node1 = None
+        self.node2 = None
+        self.logger = logging.getLogger('pyHerc.generators.utils.BSPSection')
+
+    def __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.node1 = None
+        self.node2 = None
+        self.logger = logging.getLogger('pyHerc.generators.utils.BSPSection')
+        self.logger.debug('created new BSP section with corners ' +
+                                    self.corner1.__str__() + ' and ' + self.corner2.__str__())
+
+    def split(self, minSize = (6, 6), direction = None):
+        assert(self.corner1 != None)
+        assert(self.corner2 != None)
+
+        size = (abs(self.corner1[0] - self.corner2[0]),
+                    abs(self.corner1[0] - self.corner2[0]))
+        self.logger.debug('splitting BSP section of size ' + size.__str__())
+
+        if direction == None:
+            direction = random.randint(1, 2) # 1 split horisontal, 2 split vertical
+        assert(direction in (1, 2))
+
+        if direction == 1:
+            self.logger.debug('split direction horisontal')
+            splitLocation = random.randint(minSize[1], size[1] - minSize[1])
+            self.logger.debug('split location ' + splitLocation.__str__())
+            self.node1 = BSPSection(self.corner1, (self.corner2[0], self.corner1[1] + splitLocation))
+            self.node2 = BSPSection((self.corner1[0], self.corner1[1] + splitLocation + 1),  self.corner2)
+        else:
+            self.logger.debug('split direction vertical')
+            splitLocation = random.randint(minSize[0], size[0] - minSize[0])
+            self.logger.debug('split location ' + splitLocation.__str__())
+            self.node1 = BSPSection(self.corner1, (self.corner1[0] + splitLocation, self.corner2[1]))
+            self.node2 = BSPSection((self.corner1[0] + splitLocation + 1, self.corner1[1]), self.corner2)
+
+        self.logger.debug('split performed')
---
+++ test/test_generator_utils.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   Copyright 2010 Tuukka Turto
+#
+#   This file is part of pyHerc.
+#
+#   pyHerc is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   pyHerc is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with pyHerc.  If not, see <http://www.gnu.org/licenses/>.
+
+import pyHerc
+import pyHerc.generators.utils
+
+class test_generatorUtils:
+
+    def test_simpleBSPSplit_horisontal(self):
+        section = pyHerc.generators.utils.BSPSection((0, 0), (20, 20))
+        assert(section.node1 == None)
+        assert(section.node2 == None)
+
+        section.split(direction = 1)
+        # 1 split horisontal
+        # 2 split vertical
+
+        assert(section.node1 != None)
+        assert(section.node2 != None)
+
+        assert(section.node1.corner1[0] == section.node2.corner1[0])
+        assert(section.node1.corner1[1] == 0)
+        assert(section.node1.corner2[0] == 20)
+        assert(section.node1.corner2[1] == section.node2.corner1[1] -1)
+        assert(section.node2.corner2[0] == 20)
+        assert(section.node2.corner2[1] == 20)
+
+    def test_simpleBSPSplit_vertical(self):
+        section = pyHerc.generators.utils.BSPSection((0, 0), (20, 20))
+        assert(section.node1 == None)
+        assert(section.node2 == None)
+
+        section.split(direction = 2)
+        # 1 split horisontal
+        # 2 split vertical
+
+        assert(section.node1 != None)
+        assert(section.node2 != None)
+
+        assert(section.node1.corner1[0] == 0)
+        assert(section.node1.corner1[1] == 0)
+        assert(section.node1.corner2[0] == section.node2.corner1[0] - 1)
+        assert(section.node1.corner2[1] == 20)
+        assert(section.node2.corner2[0] == 20)
+        assert(section.node2.corner2[1] == 20)
