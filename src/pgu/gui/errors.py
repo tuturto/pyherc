df7a18613c6e46c5a36f350f195a4c2333ae52c8
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-01-21 09:49:36 +0200

    moved pgu to different location

--- src/pgu/gui/errors.py
+++
@@ -1,8 +0,0 @@
-# error.py
-
-class PguError(Exception):
-    pass
-
-class StyleError(PguError):
-    pass
-
--- src/pgu/layout.py
+++
@@ -1,4 +0,0 @@
-print('pgu.layout - This module is scheduled to be removed. Please use pgu.gui.layout instead.')
-
-from pgu.gui.layout import *
-
