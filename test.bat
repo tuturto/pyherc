901303573359eceae3b82125225b07b386ec4461
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-05-19 08:53:03 +0300

    updated test.bat to run doctests too

--- test.bat
+++ test.bat
@@ -1,1 +1,6 @@
 nosetests --with-doctest --with-xunit --with-coverage --cover-html --cover-inclusive --traverse-namespace --cover-html-dir=./cover --cover-package=pyherc
+cd doc
+cd api
+call make doctest
+cd ..
+cd ..
