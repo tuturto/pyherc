0c44eaf6a03c1aac0a79917d7d00e506ebdbddf5
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-06-24 09:13:52 +0300

    working with gui

--- src/pyherc/gui/core/container.py
+++ src/pyherc/gui/core/container.py
@@ -41,38 +41,9 @@ class Container(pgu.gui.Container):
         pygame.joystick.init()
         joy = pygame.joystick.Joystick(1)
         joy.init()
-        print joy
 
         #self.connect(JOYAXISMOTION, self.joystick_event)
         # elif e.key == K_DOWN:
         # self._move_focus(0,1)
         # return True
 
-    def joystick_event(self, code, event):
-        if self.time + 250 > pygame.time.get_ticks():
-            return
-        else:
-            self.time = pygame.time.get_ticks()
-        print event
-        if event.type == JOYAXISMOTION:
-            if event.axis == 1:
-                if event.value > 0.5:
-                    self._move_focus(0, 1)
-                elif event.value < -0.5:
-                    self._move_focus(0, -1)
-
-    def send(self, code, event = None):
-        if event != None and event.type in (JOYAXISMOTION, JOYBUTTONUP):
-            self.joystick_event(code, event)
-        super(Container, self).send(code, event)
-
-    def _event(self,e):
-        super(Container, self)._event(e)
-
-    def event(self, event):
-        """
-        Handle events
-        """
-        used = super(Container, self).event(event)
-
-        return used
