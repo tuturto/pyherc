d05ed67d7f51b1ac29c9fafd808654aab786ac91
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-11-18 22:45:32 +0200

    license is not printed during import, only when running the game

--- src/herculeum/main.py
+++ src/herculeum/main.py
@@ -34,20 +34,21 @@ from herculeum.application import APP
 from PyQt4.QtGui import QPixmap, QSplashScreen
 import herculeum.gui.resources
 
-print '#   pyherc is free software: you can redistribute it and/or modify'
-print '#   it under the terms of the GNU General Public License as published by'
-print '#   the Free Software Foundation, either version 3 of the License, or'
-print '#   (at your option) any later version.'
-print '#'
-print '#   pyherc is distributed in the hope that it will be useful,'
-print '#   but WITHOUT ANY WARRANTY; without even the implied warranty of'
-print '#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-print '#   GNU General Public License for more details.'
-print '#'
-print '#   You should have received a copy of the GNU General Public License'
-print '#   along with pyherc.  If not, see <http://www.gnu.org/licenses/>.'
-
 if __name__ == "__main__":
+
+    print '#   pyherc is free software: you can redistribute it and/or modify'
+    print '#   it under the terms of the GNU General Public License as published by'
+    print '#   the Free Software Foundation, either version 3 of the License, or'
+    print '#   (at your option) any later version.'
+    print '#'
+    print '#   pyherc is distributed in the hope that it will be useful,'
+    print '#   but WITHOUT ANY WARRANTY; without even the implied warranty of'
+    print '#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
+    print '#   GNU General Public License for more details.'
+    print '#'
+    print '#   You should have received a copy of the GNU General Public License'
+    print '#   along with pyherc.  If not, see <http://www.gnu.org/licenses/>.'
+
     APP.detect_resource_directory()
 
     pixmap = QPixmap(':splash.png');
