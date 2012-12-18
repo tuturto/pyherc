435ba0b7a79b670f7fd06bd9bd8cb8e99e45d49a
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-12-18 07:00:05 +0200

    fixes to before_install

--- before_install.sh
+++ before_install.sh
@@ -1,13 +1,15 @@
 cd ..
 curl -O http://www.riverbankcomputing.co.uk/static/Downloads/sip4/sip-4.13.3.tar.gz
-tar -xvf sip-4.13.3.tar.gz
+gunzip sip-4.13.3.tar.gz
+tar -xvf sip-4.13.3.tar
 cd sip-4.13.3
 python configure.py
 make -j 4
 sudo make install
 cd ..
 curl -O http://www.riverbankcomputing.co.uk/static/Downloads/PyQt4/PyQt-x11-gpl-4.9.4.tar.gz
-tar -xvf PyQt-x11-gpl-4.9.4.tar.gz
+gunzip PyQt-x11-gpl-4.9.4.tar.gz
+tar -xvf PyQt-x11-gpl-4.9.4.tar
 cd PyQt-x11-gpl-4.9.4
 python configure.py --confirm-license
 make -j 4
