262ca66098eea6ea7c5c2cbb01a48619eb568e35
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-12-31 17:38:20 +0200

    tidied output of itemimage directive

--- src/herculeum/sphinx/items.py
+++ src/herculeum/sphinx/items.py
@@ -28,7 +28,7 @@ from docutils.parsers.rst.directives import unchanged
 from herculeum.sphinx.helpers import with_config, shutdown_application
 import os.path
 import re
-from PyQt4.QtGui import QImage
+from PyQt4.QtGui import QImage, QPainter, QColor
 from PyQt4.QtCore import Qt
 
 class ItemDescription(nodes.General, nodes.Element):
@@ -246,8 +246,11 @@ class ItemImageDirective(Image):
         surface_manager = config.surface_manager
         icon = surface_manager.get_icon(item.icon)
         img = icon.toImage()
-        new_image = img.convertToFormat(QImage.Format_RGB32,
-                                        Qt.DiffuseAlphaDither)
+        new_image = QImage(32, 32, QImage.Format_ARGB32)
+        new_image.fill(QColor(200, 200, 200))
+        painter = QPainter(new_image)
+        painter.drawPixmap(0, 0, icon)
+        painter = None
 
         new_image.save(os.path.join(folder, file_name))
 
