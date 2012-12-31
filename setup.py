c69714e5038889c6b371ee3472319f5aef9a0252
Tuukka Turto <tuukka.turto@oktaeder.net>
2012-12-31 20:59:48 +0200

    updated setup script

--- setup.py
+++ setup.py
@@ -5,10 +5,18 @@ import py2exe
 
 setup(name = 'herculeum',
       version = '0.8',
-      description = 'Small roguelike',
+      description = 'Small roguelike game',
+      long_description = 'Herculeum is a simple roguelike game, where player has to dwelve deep into a dungeon.',
       author = 'Tuukka Turto',
       author_email = 'tuukka.turto@oktaeder.net',
       url = 'https://github.com/tuturto/pyherc/',
+      classifiers = ['Development Status :: 3 - Alpha',
+                     'Intended Audience :: End Users/Desktop',
+                     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                     'Natural Language :: English',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2.7',
+                     'Topic :: Games/Entertainment :: Role-Playing'],
       windows = [{'script': 'src/herculeum/main.py',
                   'dest_base': 'herculeum',
                   'icon_resources': [(0,'resources/herculeum.ico')]
@@ -26,9 +34,10 @@ setup(name = 'herculeum',
                  'pyherc.generators.level.room',
                  'pyherc.rules', 'pyherc.rules.attack', 'pyherc.rules.consume',
                  'pyherc.rules.inventory', 'pyherc.rules.move'],
+      requires = ['decorator'],
       options = {
-                 "py2exe": {
-                            "dll_excludes": ["MSVCP90.dll"],
-                            "includes": ["sip"]
+                 'py2exe': {
+                            'dll_excludes': ['MSVCP90.dll'],
+                            'includes': ['sip']
                             }},
       package_dir = {'': 'src'})
