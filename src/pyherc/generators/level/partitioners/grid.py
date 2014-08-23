0928930c5b2d123fbd0dc32e6da7dc34695df7bb
Tuukka Turto <tuukka.turto@oktaeder.net>
2014-08-23 21:15:48 +0300

    remove unconnected sections

--- src/pyherc/generators/level/partitioners/grid.py
+++ src/pyherc/generators/level/partitioners/grid.py
@@ -91,6 +91,12 @@ class RandomConnector():
             else:
                 break
 
+        duds = [x for x in sections
+                if not is_connected(x)]
+
+        for dud in duds:
+            sections.remove(dud)
+
         return sections
 
 
